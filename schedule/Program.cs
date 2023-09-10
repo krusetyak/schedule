25ad5c301b1e5def46ce21867fda4d3503abf4c5
krusety <krusetyak0062@icloud.com>
2023-09-11 00:09:37 +0900

    Api Data Add

--- schedule/Program.cs
+++ schedule/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using schedule;
+using schedule.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
